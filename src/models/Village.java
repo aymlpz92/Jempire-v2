5b2e834d3db8ff645e7c3212578777143b1d8d27
aymlpz92 <aymericlopez.92@gmail.com>
2026-02-26 11:10:06 +0100

    feat: methodes ajouteBatiment() (#17)
    Co-authored-by: POUZET Theo <theo.pouzet@maif.fr>

--- src/models/Village.java
+++ src/models/Village.java
@@ -1,4 +1,5 @@
 package models;
+
 import models.batiments.Batiment;
 import models.batiments.Maison;
 import models.unites.Unites;
@@ -7,14 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Village {
-  List<Unites> population = new ArrayList<>();
-  List<Batiment> batiments = new ArrayList<>();
-  Ressources ressources; 
+    List<Unites> population = new ArrayList<>();
+    List<Batiment> batiments = new ArrayList<>();
+    Ressources ressources;
 
-  public Village(List<Unites> population, List<Batiment> batiments, Ressources ressources) {
-      this.population = population;
-      this.batiments = batiments;
-      this.ressources = ressources;
+    public Village(List<Unites> population, List<Batiment> batiments, Ressources ressources) {
+        this.population = population;
+        this.batiments = batiments;
+        this.ressources = ressources;
     }
 
     public void displayRessources() {
@@ -33,23 +34,23 @@ public class Village {
     }
 
     public void displayBatiments() {
-        
+
     }
 
-    public void nextDay(){
-        // Boucler sur poupulation, appeler travailler() et décrémenté food 
+    public void nextDay() {
+        // Boucler sur poupulation, appeler travailler() et décrémenté food
     }
 
-    public void afficherDetails(){
+    public void afficherDetails() {
         for (Batiment batiment : batiments) {
             System.out.println(batiment.getName());
             System.out.println("a");
         }
         System.out.println("");
-        
+
     }
 
-    public boolean isGameOver(){
+    public boolean isGameOver() {
         return false;
     }
 
@@ -58,13 +59,14 @@ public class Village {
     }
 
     public Unites ajouterUnite() {
-        Villageois villageois = new Villageois("Paul", 10, false, null, false );
+        Villageois villageois = new Villageois("Paul", 10, false, null, false);
         population.add(villageois);
         return villageois;
     }
 
-    public void ajouterBatiment(Batiment batiment) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ajouterBatiment'");
+    public Batiment ajouterBatiment(Batiment batiment) {
+        batiments.add(batiment);
+        return batiment;
     }
+
 }
